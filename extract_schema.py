21ff232ba11868e4ab97f472e7966a1f2881feec
Kanaksinh <kanaksinh@zealousweb.com>
2026-03-11 13:02:52 +0530

    Cleanup: remove unused scripts and log files

--- extract_schema.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open(r'd:\Clients\Bluetext\beyond-trust\contentful-migration\data\contentful-schema.json', 'r', encoding='utf-8') as f:
-    data = json.load(f)
-
-for ct in data.get('contentTypes', []):
-    if ct.get('sys', {}).get('id') == 'bannerImmersive':
-        print(json.dumps(ct, indent=2))
-        break
--- get_ids.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('d:\\Clients\\Bluetext\\beyond-trust\\contentful-migration\\data\\standalone-conversion.json', 'utf8');
-const matches = content.match(/"typeId":\s*(\d+)/g);
-const ids = new Set(matches.map(m => m.split(':')[1].trim()));
-console.log(Array.from(ids).sort());
